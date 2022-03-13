5a9a935afaef5eae2233817eaaadb2fbb3bac9f5
Felipev94 <felipevillalbag@hotmail.com>
2022-03-12 19:02:55 -0500

    test

--- test/flutter_utils_test.dart
+++ test/flutter_utils_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_utils/dial_codes/dial_codes_service.dart';
 void main() {
   test('get dial list', () async {
     List<DialCodeData> list = await DialCodes().getDialCodesList();
+    print(list);
     expect(list, isNotEmpty);
   });
 }
