125ff8ff9dfe1ae789bfcc6a6ca3cf25bb3a64a7
Felipev94 <felipevillalbag@hotmail.com>
2022-03-12 19:13:06 -0500

    export service

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -1,8 +1,3 @@
 library flutter_utils;
 
-import 'package:flutter_utils/dial_codes/dial_codes_service.dart';
-
-/// A Calculator.
-class Test {
-  getD() {}
-}
+export './dial_codes/dial_codes_service.dart';
